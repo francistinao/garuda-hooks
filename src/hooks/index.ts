c42e69549a7ef2de6291152526f4d90b5bffe63c
Francis Tin-ao <franfra10j@gmail.com>
2025-12-30 14:57:09 +0800

    refactor: streamline hook exports in index files
    - Removed commented-out exports from src/hooks/index.ts for clarity.
    - Updated src/hooks/browser/index.ts to include previously commented-out hooks: useMediaQuery, useNetwork, useColorScheme, useOrientation, useDocumentTitle, useFavIcon, and useHash.
    - Improved organization and accessibility of hook exports.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,21 +1,7 @@
 // storage
-// export * from '../hooks/storage/useSessionStorage'
-// export * from '../hooks/storage/useLocalStorage'
-// export * from '../hooks/storage/useCookies'
 export * from '../hooks/storage/index'
 
 // browser
-// export * from '../hooks/browser/useMediaQuery'
-// export * from '../hooks/browser/useClipboard'
-// export * from '../hooks/browser/useFullscreen'
-// export * from '../hooks/browser/useGeolocation'
-// export * from '../hooks/browser/useIdle'
-// export * from '../hooks/browser/useNetwork'
-// export * from '../hooks/browser/useColorScheme'
-// export * from '../hooks/browser/useOrientation'
-// export * from '../hooks/browser/useDocumentTitle'
-// export * from '../hooks/browser/useFavIcon'
-// export * from '../hooks/browser/useHash'
 export * from '../hooks/browser/index'
 
 // dom
